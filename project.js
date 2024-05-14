a8b1fd313dda88878224ea612587b27a5424bc3d
robsanbay <robsanbayisa@gmail.com>
2024-05-15 00:44:34 +0300

    git inshalzathon

---
+++ project.js
@@ -0,0 +1,8 @@
+//1. Despot some money
+//2. Determine number of lines to bet on
+//3. Collect a bet amount
+//4. Spine the slot machine
+//5. Check if the user their winnings
+//6. Give the user their winnings
+//7. play again
+
